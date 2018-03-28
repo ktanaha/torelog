3445763c840eeb5368333544fd1a9bae50689379
ktanaha <moutoku1984@gmail.com>
2018-03-28 15:20:08 +0900

    update no weight pattern

--- index.js
+++ index.js
@@ -11,41 +11,25 @@
 
 const Alexa = require('alexa-sdk');
 
+// 日本時間に合わせる
+process.env.TZ = "Asia/Tokyo";
+
+// STOP CANCEL
+const END_MESSAGE = '記録せずに終了します。';
+
 const states = {
     TRANINGTYPEMODE: '_TRANINGTYPEMODE',
     TRANINGAMOUNTMODE: '_TRANINGAMOUNTMODE',
     TRANINGSAVEMODE: '_TRANINGSAVEMODE',
 };
 
-exports.handler = function (event, context) {
-    const alexa = Alexa.handler(event, context);
-    //alexa.dynamoDBTableName = 'TraningRecordTable';
-    alexa.registerHandlers(handlers, weightModeHandlers, amountModeHandlers, setCountModeHandlers, saveModeHandlers);
+exports.handler = function (event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.dynamoDBTableName = 'TraningRecordTable';
+    //alexa.appID = APP_ID;
+    alexa.registerHandlers(handlers, startSessionHandlers, traningHandlers, amountHandlers);
     alexa.execute();
 };
-
-const startSessionHandlers = {
-    'StartSession': function () {
-        this.handler.state = 'TRANINGTYPEMODE';
-        this.emit(':ask', '今日の筋トレを記録します。まずはどんな種目をしたのか教えてください。');
-    },
-    'Unhandled': function() {
-        const message = '筋トレをした種目を教えてください。';
-        this.emit(':ask', message, message);
-    }   
-};
-
-const traningIntent = Alexa.CreateStateHandler(states.TRANINGWEIGHTMODE, {  
-    'TrainingIntent': function () {
-        this.handler.state = 'TRANINGAMOUNTMODE';
-        const name = this.event.request.intent.slots.Traning.value;
-        this.emit(':ask', 'ウェイトや回数、セット数を教えてください');
-    },
-    'Unhandled': function() {
-        const message = 'ウェイトや回数、セット数を教えてください';
-        this.emit(':ask', message, message);
-    } 
-});
 
 const handlers = {
     'LaunchRequest': function () {
@@ -54,114 +38,125 @@ const handlers = {
     'StartOverIntent': function () {
         this.emit('StartSession');
     },
-    'TraningIntent': function () {
-        this.handler.state = states.TRANINGWEIGHTMODE;
-        const name =this.event.request.intent.slots.Traning.value;
-        this.attributes['name'] = name;
-        this.emit(':ask', name + 'ですね。次に重さ・ウェイトをキログラム単位で教えてください。');
-    },
     'AMAZON.HelpIntent': function () {
         this.emit(':ask', '今日やった筋トレを記録します', '今日やった筋トレを記録します。');
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', '終了します。');
+        this.emit(':tell', END_MESSAGE);
     },
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', '終了します。');
+        this.emit(':tell', END_MESSAGE);
     },
-    'Unhandled': function() {
-        const message = '筋トレをした種目を教えてください。';
-        this.emit(':ask', message, message);
-    }
 };
 
-const weightModeHandlers = Alexa.CreateStateHandler(states.TRANINGWEIGHTMODE, {
+// 開始時のハンドラ
+const startSessionHandlers = {
+    'StartSession': function () {
+        this.handler.state = states.TRANINGTYPEMODE;
+        this.emit(':ask', '今日の筋トレを記録します。まずはどんな種目をしたのか教えてください。');
+    },
     'AMAZON.HelpIntent': function () {
-        this.emit(':ask', 'もう一度お願いします。', 'もう一度お願いします。');
+        const message = '今日の筋トレを記録します。まずはどんな種目をしたのか教えてください。';
+        this.emit(':ask', message, message);
     },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', '終了します。');
+    'AMAZON.CancelIntent': function ()  {
+        this.handler.state = '';
+        delete this.attributes['STATE'];
+        
+        this.emit(':tell', END_MESSAGE);
     },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', '終了します。');
+    'AMAZON.StopIntent': function ()  {
+        this.handler.state = '';
+        delete this.attributes['STATE'];
+        
+        this.emit(':tell', END_MESSAGE);
     },
-    'WeightIntent': function () {
+    'Unhandled': function() {
+        this.handler.state = '';
+        delete this.attributes['STATE'];
+        
+        const message = 'すみません、もう一度トレーニングした種目を教えてください。';
+        this.emit(':ask', message, message);
+    }   
+};
+
+// トレーニングメニューのハンドラ
+const traningHandlers = Alexa.CreateStateHandler(states.TRANINGTYPEMODE, {  
+    'TrainingIntent': function () {
         this.handler.state = states.TRANINGAMOUNTMODE;
-        const weight = this.event.request.intent.slots.Weight.value;
-        this.attributes['weight'] = weight;
-        this.emit(':ask', weight + 'キログラムですね。次に回数を教えてください。');
+        const now = new Date();
+        this.attributes['now'] = now.toString();
+        const name = {'name': this.event.request.intent.slots.Traning.value};
+        //this.attributes[now.toString()] = {};
+        this.attributes[now.toString()] = {'name': this.event.request.intent.slots.Traning.value};
+
+        this.emit(':ask', '次にウェイトや回数、セット数を教えてください');
+    },
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', '今日の筋トレを記録します。まずはどんな種目をしたのか教えてください。', '今日の筋トレを記録します。まずはどんな種目をしたのか教えてください。');
+    },
+    'AMAZON.CancelIntent': function ()  {
+        this.handler.state = '';
+        delete this.attributes['STATE'];
+
+        this.emit(':tell', END_MESSAGE);
+    },
+    'AMAZON.StopIntent': function ()  {
+        this.handler.state = '';
+        delete this.attributes['STATE'];
+
+        this.emit(':tell', END_MESSAGE);
     },
     'Unhandled': function() {
-        const message = '重さ・ウェイトをキログラム単位で教えてください';
+        const message = 'すみませんもう一度ウェイトや回数、セット数を教えてください';
         this.emit(':ask', message, message);
-    }
+    } 
 });
 
-const amountModeHandlers = Alexa.CreateStateHandler(states.TRANINGAMOUNTMODE, {
-    'AMAZON.HelpIntent': function () {
-        this.emit(':ask', 'もう一度お願いします。', 'もう一度お願いします。');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', '終了します。');
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', '終了します。');
-    },
+// ウェイト・回数・セット数のハンドラ
+const amountHandlers = Alexa.CreateStateHandler(states.TRANINGAMOUNTMODE, {  
     'AmountIntent': function () {
-        this.handler.state = states.TRANINGSETCOUNTMODE;
-        const amount = this.event.request.intent.slots.Amount.value;
-        this.attributes['amount'] = amount;
-        this.emit(':ask', amount + '回ですね。最後にセット数を教えてください。');
-    },
-    'Unhandled': function() {
-        const message = '回数を教えてください';
-        this.emit(':ask', message, message);
-    }
-});
-
-const setCountModeHandlers = Alexa.CreateStateHandler(states.TRANINGSETCOUNTMODE, {
-    'AMAZON.HelpIntent': function () {
-        this.emit(':ask', 'もう一度お願いします。', 'もう一度お願いします。');
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', '終了します。');
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', '終了します。');
-    },
-    'SetIntent': function () {
-        const setcount = this.event.request.intent.slots.SetCount.value;
-        this.attributes['setcount'] = setcount;
+        const now = this.attributes['now'];
+        const weight = this.event.request.intent.slots.Weight.value;
+        if (weight) {
+            this.attributes[now]['weight'] = this.event.request.intent.slots.Weight.value;
+            //this.attributes['weight'] = weight;
+        }
+        const count = this.event.request.intent.slots.Count.value;
+        this.attributes[now]['count'] = this.event.request.intent.slots.Count.value;
+        //this.attributes['count'] = count;
         
-        const name = this.attributes['name'];
-        const weight = this.attributes['weight'];
-        const amount = this.attributes['amount'];
+        const span = this.event.request.intent.slots.Span.value;
+        this.attributes[now]['span'] = this.event.request.intent.slots.Span.value;
+        //this.attributes['span'] = span;
         
-        this.handler.state = states.TRANINGSAVEMODE;
-        this.emit(':ask', name + 'を' + weight + 'キログラムで' + amount + '回' + setcount + 'セットですね。記録しますか？');
+        this.handler.state = '';
+        delete this.attributes['STATE'];
+        
+        if (weight) {
+            this.emit(':tell', weight + 'キロを' + count + '回、' + span + 'セットですね。記録しました！');
+        } else {
+            this.emit(':tell', count + '回、' + span + 'セットですね。記録しました！');
+        }
+        
     },
-    'Unhandled': function() {
-        const message = 'セット数を教えてください';
-        this.emit(':ask', message, message);
-    }
-});
-
-const saveModeHandlers = Alexa.CreateStateHandler(states.TRANINGSAVEMODE, {
     'AMAZON.HelpIntent': function () {
-        this.emit(':ask', 'もう一度お願いします。', 'もう一度お願いします。');
+        this.emit(':ask', '今日の筋トレを記録します。まずはどんな種目をしたのか教えてください。', '今日の筋トレを記録します。まずはどんな種目をしたのか教えてください。');
     },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', '終了します。');
+    'AMAZON.CancelIntent': function ()  {
+        this.handler.state = '';
+        this.attributes['name'];
+        delete this.attributes['STATE'];
+        delete this.attributes['name'];
+
+        this.emit(':tell', END_MESSAGE);
     },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', '終了します。');
-    },
-    'AMAZON.YesIntent': function () {
-    this.handler.state = '';
-        this.emit(':', '了解しました。記録しました。');
-    },
-    'AMAZON.NoIntent': function () {
-    this.handler.state = '';
-        this.emit(':tell', '了解しました。記録せずに終了します。');
+    'AMAZON.StopIntent': function ()  {
+        this.handler.state = '';
+        this.attributes['name']
+        delete this.attributes['STATE'];
+        delete this.attributes['name'];
+
+        this.emit(':tell', END_MESSAGE);
     },
 });
